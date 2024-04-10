b1d2a8734a4378d61f604b118d5cfaf76950cb10
= <=>
2024-04-10 11:44:02 +0200

    inizializzazione progetto bff

---
+++ src/main/java/it/gestoreattivita/bff/GestoreAttivitaBffApplication.java
@@ -0,0 +1,13 @@
+package it.gestoreattivita.bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestoreAttivitaBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestoreAttivitaBffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/gestoreattivita/bff/GestoreAttivitaBffApplicationTests.java
@@ -0,0 +1,13 @@
+package it.gestoreattivita.bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestoreAttivitaBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
