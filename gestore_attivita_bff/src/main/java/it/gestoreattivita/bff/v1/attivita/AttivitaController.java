6bed295c8834dcbc3327beadf3d2b56302021ae8
= <=>
2024-04-13 19:30:34 +0200

    Creazione Swagger

--- gestore_attivita_bff/src/main/java/it/gestoreattivita/bff/v1/attivita/AttivitaController.java
+++ gestore_attivita_bff/src/main/java/it/gestoreattivita/bff/v1/attivita/AttivitaController.java
@@ -1,5 +1,7 @@
 package it.gestoreattivita.bff.v1.attivita;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import it.gestoreattivita.bff.util.paginator.dto.PageDto;
 import it.gestoreattivita.bff.v1.attivita.dto.AttivitaDto;
 import it.gestoreattivita.bff.v1.attivita.dto.PageAttivitaResponseDto;
@@ -13,12 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("v1/attività/")
+@Tag(name = "Attività")
 public class AttivitaController {
 
     @Autowired
     private AttivitaService attivitaService;
 
 
+    @Operation(operationId = "initAttivitaPages", summary = "inizializza il paginator delle attività")
     @GetMapping("init/")
     public ResponseEntity<PageAttivitaResponseDto> init(){
 
@@ -26,6 +30,7 @@ public class AttivitaController {
 
     }
 
+    @Operation(operationId = "getAttivitaPage", summary = "ottieni la pagina del paginator")
     @GetMapping("init/{index}")
     public ResponseEntity<PageDto<AttivitaDto>> getPage(@PathVariable("index") Long i){
         return ResponseEntity.ok().body(attivitaService.getPage(i.longValue()));
